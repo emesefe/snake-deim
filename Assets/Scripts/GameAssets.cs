f4e8f70615eae6b4d850a06a399b20a4374353a5
emesefe <maria@frogames.es>
2023-10-03 09:16:38 +0200

    Game Assets script

---
+++ Assets/Scripts/GameAssets.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAssets : MonoBehaviour
+{
+    public static GameAssets Instance { get; private set; }
+
+    public Sprite snakeHeadSprite;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There is more than one Instance");
+        }
+
+        Instance = this;
+    }
+}

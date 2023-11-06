1f66b5ceaeae369243033a055e3378b2a6375234
emesefe <maria@frogames.es>
2023-11-06 12:46:08 +0100

    Game Over UI Logic

--- Assets/Scripts/GameOverUI.cs
+++ Assets/Scripts/GameOverUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,25 @@ using UnityEngine.UI;
 public class GameOverUI : MonoBehaviour
 {
     [SerializeField] private Button restartButton;
+
+    private void Awake()
+    {
+        restartButton.onClick.AddListener(() => {Loader.Load(Loader.Scene.Game);});
+        //restartButton.onClick.AddListener(RestartButtonFunction);
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
     
-    
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // private void RestartButtonFunction()
+    // {
+    //     Loader.Load(Loader.Scene.Game);
+    // }
 }

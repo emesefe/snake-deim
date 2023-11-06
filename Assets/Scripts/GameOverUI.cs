669162ad410d81ab3772e3cf1271339ad068d12e
emesefe <maria@frogames.es>
2023-11-06 12:53:40 +0100

    Game Over UI Singleton

--- Assets/Scripts/GameOverUI.cs
+++ Assets/Scripts/GameOverUI.cs
@@ -3,10 +3,20 @@ using UnityEngine.UI;
 
 public class GameOverUI : MonoBehaviour
 {
+    // Singleton
+    public static GameOverUI Instance { get; private set; }
+    
     [SerializeField] private Button restartButton;
 
     private void Awake()
     {
+        if (Instance != null)
+        {
+            Debug.LogError("More than one Instance");
+        }
+
+        Instance = this;
+        
         restartButton.onClick.AddListener(() => {Loader.Load(Loader.Scene.Game);});
         //restartButton.onClick.AddListener(RestartButtonFunction);
     }

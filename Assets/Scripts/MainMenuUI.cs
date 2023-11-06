2a94dfd80ef09fd77feea53f9194c2e50a25bd4b
emesefe <maria@frogames.es>
2023-11-06 13:29:05 +0100

    MainMenuUI

---
+++ Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button howToPlayButton;
+    [SerializeField] private Button quitButton;
+
+    private void Awake()
+    {
+        playButton.onClick.AddListener(() => {Loader.Load(Loader.Scene.Game);});
+        
+        quitButton.onClick.AddListener(() => { Application.Quit();});
+    }
+}
